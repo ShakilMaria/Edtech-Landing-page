1b7e0e42ff347960bf247e125375510ebd06e7be
Shakil <shakilahammad5800@gmail.com>
2024-09-28 09:14:09 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+      'bnt': '#54B8FF',
+      'heading' : '#2F2B71',
+      'title': '#5F596E',
+      'pera-t': '#000000',
+    },
+    extend: {
+    },
+  },
+  plugins: [],
+}
+
